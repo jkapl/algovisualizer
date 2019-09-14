be58bac4638fd22ab030b0a7836845e4655a49af
Joel <joelkaplan1@gmail.com>
2019-09-14 15:07:55 -0700

    Add server and introduce storage for intermediate sorting

--- src/algorithms/index.js
+++ src/algorithms/index.js
@@ -1,4 +1,8 @@
-const quickSort = (array) => {
+const quickSort = (array, intermediateObj, iterations) => {
+  console.log(intermediateObj)
+  // console.log(iterations)
+  // intermediateObj[iterations[iterations.length-1]] = array;
+  iterations.push(iterations[iterations.length-1]+1);
 
   if (array.length <= 1) {
     return array;
@@ -7,15 +11,17 @@ const quickSort = (array) => {
   let left = [];
   let right = [];
   let pivot = array[array.length - 1];
+  console.log(`pivot: ${pivot}`)
 
   for (let i = 0; i < array.length - 1; i++) {
     if (array[i] <= pivot) {
       left.push(array[i]);
     } else {
-      right.push(array[i])
+      right.push(array[i]);
     }
   }
-  return [...quickSort(left), pivot, ...quickSort(right)]
+  intermediateObj[iterations[iterations.length-1]] = [...left, pivot, ...right];
+  return [...quickSort(left, intermediateObj, iterations), pivot, ...quickSort(right, intermediateObj, iterations)]
 }
 
 export default quickSort;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,12 @@ import quickSort  from '../algorithms/index';
 const App = () => {
   const [algoData, setAlgoData] = useState([9,5,4,2,4,32,3]);
   const [nums, setNums] = useState([9,5,4,2,4,32,3]);
+  const intermediateObj = {};
+  const count = [0];
 
   return (
     <div>
-      <button onClick={() => { setAlgoData(quickSort(nums)) } }></button>
+      <button onClick={() => setAlgoData(quickSort(nums, intermediateObj, count))  }></button>
       {algoData.map(line => {
         return <span> - {line}</span>
       })}
